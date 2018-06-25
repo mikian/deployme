cd5a30deb223dbefd4607224f4f30e772a3624c7
Mikko Kokkonen <mikko.kokkonen@appearhere.co.uk>
2018-06-25 16:09:12 +0800

    Allow re-creating services

--- lib/core_ext/aws_ecs_client.rb
+++ lib/core_ext/aws_ecs_client.rb
@@ -2,11 +2,14 @@ require 'aws-sdk-ecs'
 
 class Aws::ECS::Client # rubocop:disable Style/ClassAndModuleChildren
   def upsert_service(options)
-    if describe_services(cluster: options[:cluster], services: [options[:service]]).services.count.zero?
+    active_services = describe_services(
+      cluster: options[:cluster], services: [options[:service]]
+    ).services.any? { |svc| svc.status == 'ACTIVE' }
+    if active_services
+      send(:update_service, options)
+    else
       options[:service_name] = options.delete(:service)
       send(:create_service, options)
-    else
-      send(:update_service, options)
     end
   end
 end
